8174e331e398ecd00cdfe61794f56e4180024e28
Wyatt Scott <122546418+WD-Scott@users.noreply.github.com>
2024-02-21 20:51:13 -0500

    Add files via upload

---
+++ BertSeqVect.py
@@ -0,0 +1,45 @@
+class BertSequenceVectorizer:
+    """ """
+    def __init__(self):
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.model_name = 'bert-base-uncased'
+        self.tokenizer = BertTokenizer.from_pretrained(self.model_name)
+        self.bert_model = transformers.BertModel.from_pretrained(self.model_name)
+        self.bert_model = self.bert_model.to(self.device)
+        self.max_len = 128
+
+    def vectorize(self, sentence : str) -> np.array:
+        """
+
+        Parameters
+        ----------
+        sentence : str :
+            
+        sentence : str :
+            
+
+        Returns
+        -------
+
+        
+        """
+        inp = self.tokenizer.encode(sentence)
+        len_inp = len(inp)
+
+        if len_inp >= self.max_len:
+            inputs = inp[:self.max_len]
+            masks = [1] * self.max_len
+        else:
+            inputs = inp + [0] * (self.max_len - len_inp)
+            masks = [1] * len_inp + [0] * (self.max_len - len_inp)
+
+        inputs_tensor = torch.tensor([inputs], dtype=torch.long).to(self.device)
+        masks_tensor = torch.tensor([masks], dtype=torch.long).to(self.device)
+
+        bert_out = self.bert_model(inputs_tensor, masks_tensor)
+        seq_out, pooled_out = bert_out['last_hidden_state'], bert_out['pooler_output']
+
+        if torch.cuda.is_available():    
+            return seq_out[0][0].cpu().detach().numpy()
+        else:
+            return seq_out[0][0].detach().numpy()
